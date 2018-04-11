c1c99dc3bd9b82f87899b7d7deac3f9829a32764
scott <myislandwifi@gmail.com>
2018-04-11 03:34:04 -0500

    asf

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -22,6 +22,7 @@ export declare module iwlib {
         screenBroken = "Screen Broken",
         unitHardReset = "Unit Hard Reset",
         other = "Other",
+        NONE = "NONE",
     }
     enum AgentActivityType {
         login = "login",
